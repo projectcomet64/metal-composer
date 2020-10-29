788c7994e3f1ba9f132546fcdce1efcfbe02ed3c
GlitchyPSI <supermglitchy3@gmail.com>
2020-10-29 17:08:10 -0400

    Bump version and fix assembly names

--- MetalComposer/Properties/AssemblyInfo.cs
+++ MetalComposer/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // La información general de un ensamblado se controla mediante el siguiente 
 // conjunto de atributos. Cambie estos valores de atributo para modificar la información
 // asociada con un ensamblado.
-[assembly: AssemblyTitle("MetalComposer")]
+[assembly: AssemblyTitle("Metal Composer")]
 [assembly: AssemblyDescription("Advanced SM64 animation trailer")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Project Comet")]
-[assembly: AssemblyProduct("MetalComposer")]
-[assembly: AssemblyCopyright("Copyright © GlitchyPSI 2019")]
+[assembly: AssemblyProduct("Metal Composer")]
+[assembly: AssemblyCopyright("Copyright © GlitchyPSI 2019-2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // Puede especificar todos los valores o usar los números de compilación y de revisión predeterminados
 // mediante el carácter "*", como se muestra a continuación:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
